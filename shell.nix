0f47b8b46b48089952f959e5fff8c721cb7b83c1
steve-chavez <stevechavezast@gmail.com>
2021-10-20 22:38:57 -0500

    nix: add valgrind pg setup

--- shell.nix
+++ shell.nix
@@ -9,15 +9,23 @@ mkShell {
   buildInputs =
     let
       pgWithExt = { pg }: pg.withPackages (p: [ (callPackage ./nix/pg_net.nix { postgresql = pg;}) ]);
-      net-with-pg-12 = callPackage ./nix/pgScript.nix { postgresql = pgWithExt { pg = postgresql_12;}; };
-      net-with-pg-13 = callPackage ./nix/pgScript.nix { postgresql = pgWithExt { pg = postgresql_13;}; };
+      pg12WithExt = pgWithExt { pg = postgresql_12;};
+      pg13WithExt = pgWithExt { pg = postgresql_13;};
+      net-with-pg-12 = callPackage ./nix/pgScript.nix { postgresql = pg12WithExt; };
+      net-with-pg-13 = callPackage ./nix/pgScript.nix { postgresql = pg13WithExt; };
+      valgrind-net-with-pg-12 = callPackage ./nix/pgValgrindScript.nix { postgresql = pg12WithExt; };
       pythonDeps = with pythonPackages; [
         pytest
         psycopg2
         sqlalchemy
       ];
     in
-    [ net-with-pg-12 net-with-pg-13 pythonDeps ];
+    [
+      net-with-pg-12
+      net-with-pg-13
+      valgrind-net-with-pg-12
+      pythonDeps
+    ];
   shellHook = ''
     export NIX_PATH="nixpkgs=${nixpkgs}:."
     export NIXOPS_STATE=".deployment.nixops"
