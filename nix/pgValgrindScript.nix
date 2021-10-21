569bb7c740c1621446d2f99aaadbbaa31043445b
steve-chavez <stevechavezast@gmail.com>
2021-10-20 22:38:57 -0500

    nix: add valgrind pg setup

---
+++ nix/pgValgrindScript.nix
@@ -0,0 +1,31 @@
+{ postgresql, valgrind, writeShellScriptBin } :
+
+let
+  LOGMIN = builtins.getEnv "LOGMIN";
+  ver = builtins.head (builtins.splitVersion postgresql.version);
+  valgrindLogFile = "valgrindlog";
+  script = ''
+    export PATH=${postgresql}/bin:"$PATH"
+
+    tmpdir="$(mktemp -d)"
+
+    export PGDATA="$tmpdir"
+    export PGHOST="$tmpdir"
+    export PGUSER=postgres
+    export PGDATABASE=postgres
+
+    PGTZ=UTC initdb --no-locale --encoding=UTF8 --nosync -U "$PGUSER"
+
+    rm ${valgrindLogFile}
+
+    echo -e "Connect to this db from another shell using the following command: \n"
+    echo -e "\t psql -h "$tmpdir" -U postgres \n"
+
+    ${valgrind}/bin/valgrind --log-file=${valgrindLogFile} \
+    --leak-check=yes --trace-children=yes --track-origins=yes --suppressions=${./valgrind.supp} \
+    postgres -c shared_preload_libraries="pg_net" -c listen_addresses="" -k $PGDATA
+
+    rm -rf "$tmpdir"
+  '';
+in
+writeShellScriptBin "valgrind-net-with-pg-${ver}" script
