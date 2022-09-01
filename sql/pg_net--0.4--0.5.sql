c8897fa1fc7295c9110fb54f47979cdeed134f68
steve-chavez <stevechavezast@gmail.com>
2022-08-31 23:06:52 -0500

    Remove primary keys from net tables
    To remove the possibility of them being used as FKs
    
    Fixes https://github.com/supabase/pg_net/issues/44

--- sql/pg_net--0.4--0.5.sql
+++ sql/pg_net--0.4--0.5.sql
@@ -1,1 +1,3 @@
 alter function net._encode_url_with_params_array ( text, text[]) strict;
+alter table net.http_request_queue drop constraint http_request_queue_pkey;
+alter table net._http_response drop constraint _http_response_pkey;
