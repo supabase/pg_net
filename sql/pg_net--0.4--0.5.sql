2cfda447975f07de61c6843351bf22e373c95993
steve-chavez <stevechavezast@gmail.com>
2022-08-31 23:26:24 -0500

    Remove index on _http_response.created
    The index doesn't get used on the DELETE FROM net._http_response in
    worker.c anyway because `clock_timestamp()` forces a Seq Scan.

--- sql/pg_net--0.4--0.5.sql
+++ sql/pg_net--0.4--0.5.sql
@@ -3,3 +3,4 @@ alter table net.http_request_queue drop constraint http_request_queue_pkey;
 alter table net._http_response drop constraint _http_response_pkey;
 drop trigger ensure_worker_is_up on net.http_request_queue;
 alter function net._check_worker_is_up() rename to check_worker_is_up;
+drop index net._http_response_created_idx;
