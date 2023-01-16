db0fdb9fb4ed2aba2095c6cee9af3383177e5a7c
steve-chavez <stevechavezast@gmail.com>
2023-01-16 14:31:52 -0500

    nix: add pg 14 and 15
    - add .history file
    - DRY use a single function for different pg versions
    - update cachix
    - test all pg versions on CI

--- shell.nix
+++ shell.nix
@@ -11,16 +11,19 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/2a058487cb7a50e7650f1657ee0151a19c59ec3b.tar.gz";
     sha256 = "1h8c0mk6jlxdmjqch6ckj30pax3hqh6kwjlvp2021x3z4pdzrn9p";
   };
+  newPkgs = (import (builtins.fetchTarball {
+    name = "2022-10-14";
+    url = "https://github.com/NixOS/nixpkgs/archive/cc090d2b942f76fad83faf6e9c5ed44b73ba7114.tar.gz";
+    sha256 = "0a1wwpbn2f38pcays6acq1gz19vw4jadl8yd3i3cd961f1x2vdq2";
+  }){});
 in with import nixpkgs {};
 mkShell {
   buildInputs =
     let
+      supportedPgVersions = [ postgresql_12 postgresql_13 newPkgs.postgresql_14 newPkgs.postgresql_15 ];
       pgWithExt = { pg }: pg.withPackages (p: [ (callPackage ./nix/pg_net.nix { postgresql = pg;}) ]);
-      pg12WithExt = pgWithExt { pg = postgresql_12;};
-      pg13WithExt = pgWithExt { pg = postgresql_13;};
-      net-with-pg-12 = callPackage ./nix/pgScript.nix { postgresql = pg12WithExt; };
-      net-with-pg-13 = callPackage ./nix/pgScript.nix { postgresql = pg13WithExt; };
-      valgrind-net-with-pg-12 = callPackage ./nix/pgValgrindScript.nix { postgresql = pg12WithExt; };
+      extAll = map (x: callPackage ./nix/pgScript.nix { postgresql = pgWithExt { pg = x;}; }) supportedPgVersions;
+      valgrind-net-with-pg-12 = callPackage ./nix/pgValgrindScript.nix { postgresql = pgWithExt { pg = postgresql_12;}; };
       nginxScript = callPackage ./nix/nginxScript.nix { openresty = oldOpenresty; };
       pathodScript = callPackage ./nix/pathodScript.nix {};
       pythonDeps = with pythonPackages; [
@@ -31,9 +34,8 @@ mkShell {
       format = callPackage ./nix/format.nix {};
     in
     [
-      net-with-pg-12
-      net-with-pg-13
       valgrind-net-with-pg-12
+      extAll
       pythonDeps
       nixops
       format.do format.doCheck
@@ -43,5 +45,6 @@ mkShell {
   shellHook = ''
     export NIX_PATH="nixpkgs=${nixpkgs}:."
     export NIXOPS_STATE=".deployment.nixops"
+    export HISTFILE=.history
   '';
 }
