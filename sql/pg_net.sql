8c059af7f86ea711c4b80d660bc37cd238c3b983
steve-chavez <stevechavezast@gmail.com>
2023-02-07 01:08:01 -0500

    fix: restore the net.http_collect_response
    mark it as deprecated

--- sql/pg_net.sql
+++ sql/pg_net.sql
@@ -332,5 +332,25 @@ begin
 end;
 $$;
 
+create or replace function net.http_collect_response(
+    -- request_id reference
+    request_id bigint,
+    -- when `true`, return immediately. when `false` wait for the request to complete before returning
+    async bool default true
+)
+    -- http response composite wrapped in a result type
+    returns net.http_response_result
+    strict
+    volatile
+    parallel safe
+    language plpgsql
+    security definer
+as $$
+begin
+  raise notice 'The net.http_collect_response function is deprecated.';
+  select net._http_collect_response(request_id, async);
+end;
+$$;
+
 grant all on schema net to postgres;
 grant all on all tables in schema net to postgres;
