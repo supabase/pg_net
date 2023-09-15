7021cf4d7678a6243b47710712843d18fc70250e
steve-chavez <stevechavezast@gmail.com>
2023-09-15 17:31:42 -0300

    fix: check_worker_is_up
    * Test it works, previously it only tested failure.

--- sql/pg_net.sql
+++ sql/pg_net.sql
@@ -20,7 +20,7 @@ create unlogged table net.http_request_queue(
 
 create or replace function net.check_worker_is_up() returns void as $$
 begin
-  if not exists (select pid from pg_stat_activity where backend_type = 'pg_net worker') then
+  if not exists (select pid from pg_stat_activity where backend_type ilike '%pg_net%') then
     raise exception using
       message = 'the pg_net background worker is not up'
     , detail  = 'the pg_net background worker is down due to an internal error and cannot process requests'
@@ -28,6 +28,7 @@ begin
   end if;
 end
 $$ language plpgsql;
+comment on function net.check_worker_is_up() is 'raises an exception if the pg_net background worker is not up, otherwise it doesn''t return anything';
 
 -- Associates a response with a request
 -- API: Private
--- test/test_worker_error.py
+++ test/test_worker_error.py
@@ -2,12 +2,23 @@ from sqlalchemy import text
 import pytest
 import time
 
+def test_success_when_worker_is_up(sess):
+    """net.check_worker_is_up should not return anything when the worker is running"""
+
+    (result,) = sess.execute(
+        """
+        select net.check_worker_is_up();
+        """
+    ).fetchone()
+    assert result is not None
+    assert result == ''
+
 def test_http_get_error_when_worker_down(sess):
     """net.http_get returns an error when pg background worker is down"""
 
     # kill the bg worker manually
     sess.execute(text("""
-        select pg_terminate_backend((select pid from pg_stat_activity where backend_type = 'pg_net worker'));
+        select pg_terminate_backend((select pid from pg_stat_activity where backend_type ilike '%pg_net%'));
     """))
 
     time.sleep(1);
