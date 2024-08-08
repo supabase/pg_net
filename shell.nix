4e2b9f23ad1391eff352f2e9ebc3f689b37d211b
steve-chavez <stevechavezast@gmail.com>
2024-08-08 00:46:27 -0500

    chore: use latest stable nixpkgs globally
    - Remove nixops as it's incompatible with this nixpkgs version

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
 let
+  oldNixpkgs = (import (builtins.fetchTarball {
+    name = "2020-04-20";
+    url = "https://github.com/NixOS/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  }){});
   nixpkgs = builtins.fetchTarball {
-    name = "2020-12-22";
-    url = "https://github.com/NixOS/nixpkgs/archive/2a058487cb7a50e7650f1657ee0151a19c59ec3b.tar.gz";
-    sha256 = "1h8c0mk6jlxdmjqch6ckj30pax3hqh6kwjlvp2021x3z4pdzrn9p";
-  };
-  newPkgs = (import (builtins.fetchTarball {
-    name = "24.05";
+    name = "24.05"; # May 31 2024
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
     sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  }){});
+  };
 in with import nixpkgs {};
 mkShell {
   buildInputs =
@@ -16,15 +16,15 @@ mkShell {
       supportedPgVersions = [
         postgresql_12
         postgresql_13
-        newPkgs.postgresql_14
-        newPkgs.postgresql_15
-        newPkgs.postgresql_16
+        postgresql_14
+        postgresql_15
+        postgresql_16
       ];
       pgWithExt = { pg }: pg.withPackages (p: [ (callPackage ./nix/pg_net.nix { postgresql = pg;}) ]);
       extAll = map (x: callPackage ./nix/pgScript.nix { postgresql = pgWithExt { pg = x;}; }) supportedPgVersions;
-      nginxScript = callPackage ./nix/nginxScript.nix { nginx = newPkgs.nginx; nginxModules = newPkgs.nginxModules; };
-      pathodScript = callPackage ./nix/pathodScript.nix {};
-      pythonDeps = with pythonPackages; [
+      nginxScript = callPackage ./nix/nginxScript.nix {};
+      pathodScript = callPackage ./nix/pathodScript.nix { mitmproxy = oldNixpkgs.mitmproxy; };
+      pythonDeps = with python3Packages; [
         pytest
         psycopg2
         sqlalchemy
@@ -34,14 +34,11 @@ mkShell {
     [
       extAll
       pythonDeps
-      nixops
       format.do format.doCheck
       nginxScript
       pathodScript
     ];
   shellHook = ''
-    export NIX_PATH="nixpkgs=${nixpkgs}:."
-    export NIXOPS_STATE=".deployment.nixops"
     export HISTFILE=.history
   '';
 }
