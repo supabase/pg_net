8f821fb63d2f97adfb3136f8a7cca10a628659d9
steve-chavez <stevechavezast@gmail.com>
2024-09-04 22:44:24 -0500

    chore: remove pathod and use a single nixpkgs
    Closes https://github.com/supabase/pg_net/issues/106

--- shell.nix
+++ shell.nix
@@ -1,15 +1,8 @@
-let
-  oldNixpkgs = (import (builtins.fetchTarball {
-    name = "2020-04-20";
-    url = "https://github.com/NixOS/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz";
-    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
-  }){});
-  nixpkgs = builtins.fetchTarball {
+with import (builtins.fetchTarball {
     name = "24.05"; # May 31 2024
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
     sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  };
-in with import nixpkgs {};
+}) {};
 mkShell {
   buildInputs =
     let
@@ -23,7 +16,6 @@ mkShell {
       pgWithExt = { pg }: pg.withPackages (p: [ (callPackage ./nix/pg_net.nix { postgresql = pg;}) ]);
       extAll = map (x: callPackage ./nix/pgScript.nix { postgresql = pgWithExt { pg = x;}; }) supportedPgVersions;
       nginxScript = callPackage ./nix/nginxScript.nix {};
-      pathodScript = callPackage ./nix/pathodScript.nix { mitmproxy = oldNixpkgs.mitmproxy; };
       gdbScript = callPackage ./nix/gdbScript.nix {};
       pythonDeps = with python3Packages; [
         pytest
@@ -37,7 +29,6 @@ mkShell {
       pythonDeps
       format.do format.doCheck
       nginxScript
-      pathodScript
       gdbScript
     ];
   shellHook = ''
