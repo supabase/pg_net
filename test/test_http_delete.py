c9c521a069d02d0eb29f6b6205eec8bd4010fe38
steve-chavez <stevechavezast@gmail.com>
2024-12-11 20:01:28 -0500

    test: positional arguments on net.http_delete
    Prepares for adding a body parameter to the net.http_delete function.

--- test/test_http_delete.py
+++ test/test_http_delete.py
@@ -15,7 +15,7 @@ def test_http_delete_returns_id(sess):
 
 
 def test_http_delete_collect_sync_success(sess):
-    """Collect a response, waiting if it has not completed yet"""
+    """test net.http_delete works"""
 
     # Create a request
     (request_id,) = sess.execute(text(
@@ -47,3 +47,116 @@ def test_http_delete_collect_sync_success(sess):
     assert response[2] is not None
     assert "X-Baz" in response[2]
     assert "param-foo" in response[2]
+
+
+def test_http_delete_positional_args(sess):
+    """test net.http_delete works with positional arguments. This to ensure backwards compat when a new parameter is added to the function."""
+
+    (request_id,) = sess.execute(text(
+        """
+        select net.http_delete(
+            'http://localhost:8080/delete'
+        );
+    """
+    )).fetchone()
+
+    # Commit so background worker can start
+    sess.commit()
+
+    # Collect the response, waiting as needed
+    response = sess.execute(
+        text(
+            """
+        select * from net._http_collect_response(:request_id, async:=false);
+    """
+        ),
+        {"request_id": request_id},
+        ).fetchone()
+
+    assert response is not None
+    assert response[0] == "SUCCESS"
+    assert response[1] == "ok"
+
+
+    (request_id,) = sess.execute(text(
+        """
+        select net.http_delete(
+            'http://localhost:8080/delete',
+            '{"param-foo": "bar"}'
+        );
+    """
+    )).fetchone()
+
+    # Commit so background worker can start
+    sess.commit()
+
+    # Collect the response, waiting as needed
+    response = sess.execute(
+        text(
+            """
+        select * from net._http_collect_response(:request_id, async:=false);
+    """
+        ),
+        {"request_id": request_id},
+        ).fetchone()
+
+    assert response is not None
+    assert response[0] == "SUCCESS"
+    assert response[1] == "ok"
+
+
+    (request_id,) = sess.execute(text(
+        """
+        select net.http_delete(
+            'http://localhost:8080/delete',
+            '{"param-foo": "bar"}',
+            '{"X-Baz": "foo"}'
+        );
+    """
+    )).fetchone()
+
+    # Commit so background worker can start
+    sess.commit()
+
+    # Collect the response, waiting as needed
+    response = sess.execute(
+        text(
+            """
+        select * from net._http_collect_response(:request_id, async:=false);
+    """
+        ),
+        {"request_id": request_id},
+        ).fetchone()
+
+    assert response is not None
+    assert response[0] == "SUCCESS"
+    assert response[1] == "ok"
+
+
+    (request_id,) = sess.execute(text(
+        """
+        select net.http_delete(
+            'http://localhost:8080/delete',
+            '{"param-foo": "bar"}',
+            '{"X-Baz": "foo"}',
+            5000
+        );
+    """
+    )).fetchone()
+
+    # Commit so background worker can start
+    sess.commit()
+
+    # Collect the response, waiting as needed
+    response = sess.execute(
+        text(
+            """
+        select * from net._http_collect_response(:request_id, async:=false);
+    """
+        ),
+        {"request_id": request_id},
+        ).fetchone()
+
+    assert response is not None
+    assert response[0] == "SUCCESS"
+    assert response[1] == "ok"
