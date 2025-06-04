17fbedc60e919264af34ed2f272a8d3db42bbd9c
steve-chavez <stevechavezast@gmail.com>
2025-06-03 22:09:13 -0500

    test: bgworker does queries unnecessarily
    Proves the issue on https://github.com/supabase/pg_net/issues/164,
    the bgworker is polluting `pg_stat_statements` with lots of queries
    unnecessarily.

---
+++ test/test_stat_statements.py
@@ -0,0 +1,58 @@
+import time
+
+import pytest
+from sqlalchemy import text
+
+def test_query_stat_statements(sess):
+    """Check that the background worker executes queries despite no new requests arriving"""
+
+    (pg_version,) = sess.execute(text(
+        """
+        select current_setting('server_version_num');
+    """
+    )).fetchone()
+
+    if int(pg_version) < 140000:
+        pytest.skip("Skipping fixture on pg version < 14. The query_id column on pg_stat_statements is only available on >= 14")
+
+    sess.execute(text(
+        """
+        create extension pg_stat_statements;
+    """
+    ))
+
+    sess.commit()
+
+    time.sleep(1)
+
+    (old_calls,) = sess.execute(text(
+        """
+        select
+            pss.calls
+        from
+            pg_stat_activity sa
+        join
+            pg_stat_statements pss
+            on sa.query_id = pss.queryid
+        where
+            sa.backend_type ilike '%pg_net%';
+    """
+    )).fetchone()
+
+    time.sleep(3)
+
+    (new_calls,) = sess.execute(text(
+        """
+        select
+            pss.calls
+        from
+            pg_stat_activity sa
+        join
+            pg_stat_statements pss
+            on sa.query_id = pss.queryid
+        where
+            sa.backend_type ilike '%pg_net%';
+    """
+    )).fetchone()
+
+    assert new_calls > old_calls
