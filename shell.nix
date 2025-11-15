239bc90392f9848403d225bb0b938d870bc42dee
steve-chavez <stevechavezast@gmail.com>
2025-11-14 19:35:10 -0500

    fix: compile errors with clang 21
    Upgrade to nixpkgs 2025-11-13 to obtain clang 21.
    
    * compile with Wdangling-assignment from clang 21
    * fix EREPORT_CURL_MULTI_SETOPT compile failures due to type mismatch

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import (builtins.fetchTarball {
-    name = "25.05";
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
-    sha256 = "sha256:1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
+  name = "2025-11-13";
+  url = "https://github.com/NixOS/nixpkgs/archive/91c9a64ce2a84e648d0cf9671274bb9c2fb9ba60.tar.gz";
+  sha256 = "sha256:19myp93spfsf5x62k6ncan7020bmbn80kj4ywcykqhb9c3q8fdr1";
 }) {};
 mkShell {
   buildInputs =
