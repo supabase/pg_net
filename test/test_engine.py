f3cf23495712763337ad6c96509e6d54330ebd0c
Raminder Singh <romi_ssk@yahoo.co.in>
2026-07-29 20:32:22 +0530

    tests: add test description

--- test/test_engine.py
+++ test/test_engine.py
@@ -1,5 +1,6 @@
 from sqlalchemy import text
 
 def test_connect(sess):
+    """sanity test checking that connection to postgres works"""
     (x,) = sess.execute(text("select 1")).fetchone()
     assert x == 1
