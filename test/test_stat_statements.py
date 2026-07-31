a64f20d5d59771834d8feb5468f848dcfe037afd
Raminder Singh <romi_ssk@yahoo.co.in>
2026-07-31 18:45:18 +0530

    refactor: factor out common code into functions

--- test/test_stat_statements.py
+++ test/test_stat_statements.py
@@ -5,12 +5,7 @@ from sqlalchemy import text
 from common import http_request
 
 
-def test_query_stat_statements(sess):
-    """
-    Check that the background worker doesn't execute
-    queries when no new requests arrive
-    """
-
+def skip_test_if_pg_is_old(sess):
     (pg_version,) = sess.execute(text(
         """
         select current_setting('server_version_num');
@@ -21,6 +16,8 @@ def test_query_stat_statements(sess):
         pytest.skip(
             "Skipping fixture on pg version < 14. The query_id column on pg_stat_statements is only available on >= 14")
 
+
+def create_pg_stat_statement(sess):
     sess.execute(text(
         """
         create extension pg_stat_statements;
@@ -29,31 +26,8 @@ def test_query_stat_statements(sess):
 
     sess.commit()
 
-    (old_calls,) = sess.execute(text(
-        """
-        select coalesce(sum(calls), 0)
-        from pg_stat_statements
-        where
-            query ilike '%DELETE FROM net._http_response r %' or
-            query ilike '%DELETE FROM net.http_request_queue%';
-    """
-    )).fetchone()
 
-    # sleep for some time to see if new queries arrive
-    time.sleep(3)
-
-    (new_calls,) = sess.execute(text(
-        """
-        select coalesce(sum(calls), 0)
-        from pg_stat_statements
-        where
-            query ilike '%DELETE FROM net._http_response r %' or
-            query ilike '%DELETE FROM net.http_request_queue%';
-    """
-    )).fetchone()
-
-    assert new_calls == old_calls
-
+def drop_pg_stat_statement(sess):
     sess.execute(text(
         """
         select pg_stat_statements_reset();
@@ -64,34 +38,8 @@ def test_query_stat_statements(sess):
     sess.commit()
 
 
-def test_wakes_at_commit_time(sess):
-    """
-    Check that the background worker only does one wake at
-    commit time, avoiding unnecessary wakes and work
-    """
-
-    (pg_version,) = sess.execute(text(
-        """
-        select current_setting('server_version_num');
-    """
-    )).fetchone()
-
-    if int(pg_version) < 140000:
-        pytest.skip(
-            "Skipping fixture on pg version < 14. The query_id column on pg_stat_statements is only available on >= 14")
-
-    sess.execute(text(
-        """
-        create extension pg_stat_statements;
-    """
-    ))
-
-    sess.commit()
-
-    # wait for initial queries
-    time.sleep(1)
-
-    (initial_calls,) = sess.execute(text(
+def get_worker_query_count(sess):
+    (count,) = sess.execute(text(
         """
         select coalesce(sum(calls), 0)
         from pg_stat_statements
@@ -100,6 +48,46 @@ def test_wakes_at_commit_time(sess):
             query ilike '%DELETE FROM net.http_request_queue%';
     """
     )).fetchone()
+
+    return count
+
+
+def test_query_stat_statements(sess):
+    """
+    Check that the background worker doesn't execute
+    queries when no new requests arrive
+    """
+
+    skip_test_if_pg_is_old(sess)
+
+    create_pg_stat_statement(sess)
+
+    old_calls = get_worker_query_count(sess)
+
+    # sleep for some time to see if new queries arrive
+    time.sleep(3)
+
+    new_calls = get_worker_query_count(sess)
+
+    assert new_calls == old_calls
+
+    drop_pg_stat_statement(sess)
+
+
+def test_wakes_at_commit_time(sess):
+    """
+    Check that the background worker only does one wake at
+    commit time, avoiding unnecessary wakes and work
+    """
+
+    skip_test_if_pg_is_old(sess)
+
+    create_pg_stat_statement(sess)
+
+    # wait for initial queries
+    time.sleep(1)
+
+    initial_calls = get_worker_query_count(sess)
 
     assert initial_calls >= 0
 
@@ -112,15 +100,7 @@ def test_wakes_at_commit_time(sess):
     # wait for reqs
     time.sleep(2)
 
-    (commit_calls,) = sess.execute(text(
-        """
-        select coalesce(sum(calls), 0)
-        from pg_stat_statements
-        where
-            query ilike '%DELETE FROM net._http_response r %' or
-            query ilike '%DELETE FROM net.http_request_queue%';
-    """
-    )).fetchone()
+    commit_calls = get_worker_query_count(sess)
 
     # only 4 queries should be made for the above requests
     # 2 queries at wake, 2 extra to check if there are more rows to be processed
@@ -138,23 +118,8 @@ def test_wakes_at_commit_time(sess):
     # wait for requests
     time.sleep(2)
 
-    (rollback_calls,) = sess.execute(text(
-        """
-        select coalesce(sum(calls), 0)
-        from pg_stat_statements
-        where
-            query ilike '%DELETE FROM net._http_response r %' or
-            query ilike '%DELETE FROM net.http_request_queue%';
-    """
-    )).fetchone()
+    rollback_calls = get_worker_query_count(sess)
 
     assert rollback_calls == commit_calls
 
-    sess.execute(text(
-        """
-        select pg_stat_statements_reset();
-        drop extension pg_stat_statements;
-    """
-    ))
-
-    sess.commit()
+    drop_pg_stat_statement(sess)
