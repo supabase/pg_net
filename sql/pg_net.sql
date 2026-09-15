5256905d866e4eef21ed23a0111a4542f1c4bd90
Andrew Jackson <andrew.jackson@supabase.io>
2026-09-15 09:22:19 -0500

    remove net from upgrade script

--- sql/pg_net.sql
+++ sql/pg_net.sql
@@ -1,6 +1,5 @@
-create schema if not exists net;
 
-create domain net.http_method as text
+create domain http_method as text
 check (
   value ilike 'get'
   or value ilike 'post'
@@ -9,16 +8,16 @@ check (
 
 -- Store pending requests. The background worker reads from here
 -- API: Private
-create unlogged table net.http_request_queue(
+create unlogged table http_request_queue(
     id bigserial,
-    method net.http_method not null,
+    method http_method not null,
     url text not null,
     headers jsonb,
     body bytea,
     timeout_milliseconds int not null
 );
 
-create or replace function net.check_worker_is_up() returns void as $$
+create or replace function check_worker_is_up() returns void as $$
 begin
   if not exists (select pid from pg_stat_activity where backend_type ilike '%pg_net%') then
     raise exception using
@@ -28,11 +27,11 @@ begin
   end if;
 end
 $$ language plpgsql;
-comment on function net.check_worker_is_up() is 'raises an exception if the pg_net background worker is not up, otherwise it doesn''t return anything';
+comment on function check_worker_is_up() is 'raises an exception if the pg_net background worker is not up, otherwise it doesn''t return anything';
 
 -- Associates a response with a request
 -- API: Private
-create unlogged table net._http_response(
+create unlogged table _http_response(
     id bigint,
     status_code integer,
     content_type text,
@@ -43,23 +42,23 @@ create unlogged table net._http_response(
     created timestamptz not null default now()
 );
 
-create index on net._http_response (created);
+create index on _http_response (created);
 
 -- Blocks until an http_request is complete
 -- API: Private
-create or replace function net._await_response(
+create or replace function _await_response(
     request_id bigint
 )
     returns bool
     language plpgsql
 as $$
 declare
-    rec net._http_response;
+    rec _http_response;
 begin
     while rec is null loop
         select *
         into rec
-        from net._http_response
+        from _http_response
         where id = request_id;
 
         if rec is null then
@@ -75,7 +74,7 @@ $$;
 
 -- url encode a string
 -- API: Private
-create or replace function net._urlencode_string(string varchar)
+create or replace function _urlencode_string(string varchar)
     -- url encoded string
     returns text
     language 'c'
@@ -83,32 +82,32 @@ create or replace function net._urlencode_string(string varchar)
 as 'MODULE_PATHNAME';
 
 -- API: Private
-create or replace function net._encode_url_with_params_array(url text, params_array text[])
+create or replace function _encode_url_with_params_array(url text, params_array text[])
     -- url encoded string
     returns text
     language 'c'
     immutable
 as 'MODULE_PATHNAME';
 
-create or replace function net.worker_restart()
+create or replace function worker_restart()
   returns bool
   language 'c'
 as 'MODULE_PATHNAME';
 
-create or replace function net.wait_until_running()
+create or replace function wait_until_running()
   returns void
   language 'c'
 as 'MODULE_PATHNAME';
-comment on function net.wait_until_running() is 'waits until the worker is running';
+comment on function wait_until_running() is 'waits until the worker is running';
 
-create or replace function net.wake()
+create or replace function wake()
   returns void
   language 'c'
 as 'MODULE_PATHNAME';
 
 -- Interface to make an async request
 -- API: Public
-create or replace function net.http_get(
+create or replace function http_get(
     -- url for the request
     url text,
     -- key/value pairs to be url encoded and appended to the `url`
@@ -126,22 +125,22 @@ declare
     request_id bigint;
     params_array text[];
 begin
-    select coalesce(array_agg(net._urlencode_string(key) || '=' || net._urlencode_string(value)), '{}')
+    select coalesce(array_agg(_urlencode_string(key) || '=' || _urlencode_string(value)), '{}')
     into params_array
     from jsonb_each_text(params);
 
     -- Add to the request queue
-    insert into net.http_request_queue(method, url, headers, timeout_milliseconds)
+    insert into http_request_queue(method, url, headers, timeout_milliseconds)
     values (
         'GET',
-        net._encode_url_with_params_array(url, params_array),
+        _encode_url_with_params_array(url, params_array),
         headers,
         timeout_milliseconds
     )
     returning id
     into request_id;
 
-    perform net.wake();
+    perform wake();
 
     return request_id;
 end
@@ -149,7 +148,7 @@ $$;
 
 -- Interface to make an async request
 -- API: Public
-create or replace function net.http_post(
+create or replace function http_post(
     -- url for the request
     url text,
     -- body of the POST request
@@ -193,17 +192,17 @@ begin
     end if;
 
     select
-        coalesce(array_agg(net._urlencode_string(key) || '=' || net._urlencode_string(value)), '{}')
+        coalesce(array_agg(_urlencode_string(key) || '=' || _urlencode_string(value)), '{}')
     into
         params_array
     from
         jsonb_each_text(params);
 
     -- Add to the request queue
-    insert into net.http_request_queue(method, url, headers, body, timeout_milliseconds)
+    insert into http_request_queue(method, url, headers, body, timeout_milliseconds)
     values (
         'POST',
-        net._encode_url_with_params_array(url, params_array),
+        _encode_url_with_params_array(url, params_array),
         headers,
         convert_to(body::text, 'UTF8'),
         timeout_milliseconds
@@ -211,7 +210,7 @@ begin
     returning id
     into request_id;
 
-    perform net.wake();
+    perform wake();
 
     return request_id;
 end
@@ -219,7 +218,7 @@ $$;
 
 -- Interface to make an async request
 -- API: Public
-create or replace function net.http_delete(
+create or replace function http_delete(
     -- url for the request
     url text,
     -- key/value pairs to be url encoded and appended to the `url`
@@ -239,15 +238,15 @@ declare
     request_id bigint;
     params_array text[];
 begin
-    select coalesce(array_agg(net._urlencode_string(key) || '=' || net._urlencode_string(value)), '{}')
+    select coalesce(array_agg(_urlencode_string(key) || '=' || _urlencode_string(value)), '{}')
     into params_array
     from jsonb_each_text(params);
 
     -- Add to the request queue
-    insert into net.http_request_queue(method, url, headers, body, timeout_milliseconds)
+    insert into http_request_queue(method, url, headers, body, timeout_milliseconds)
     values (
         'DELETE',
-        net._encode_url_with_params_array(url, params_array),
+        _encode_url_with_params_array(url, params_array),
         headers,
         convert_to(body::text, 'UTF8'),
         timeout_milliseconds
@@ -255,7 +254,7 @@ begin
     returning id
     into request_id;
 
-    perform net.wake();
+    perform wake();
 
     return request_id;
 end
@@ -263,12 +262,12 @@ $$;
 
 -- Lifecycle states of a request (all protocols)
 -- API: Public
-create type net.request_status as enum ('PENDING', 'SUCCESS', 'ERROR');
+create type request_status as enum ('PENDING', 'SUCCESS', 'ERROR');
 
 
 -- A response from an HTTP server
 -- API: Public
-create type net.http_response AS (
+create type http_response AS (
     status_code integer,
     headers jsonb,
     body text
@@ -276,37 +275,37 @@ create type net.http_response AS (
 
 -- State wrapper around responses
 -- API: Public
-create type net.http_response_result as (
-    status net.request_status,
+create type http_response_result as (
+    status request_status,
     message text,
-    response net.http_response
+    response http_response
 );
 
 
 -- Collect respones of an http request
 -- API: Private
-create or replace function net._http_collect_response(
+create or replace function _http_collect_response(
     -- request_id reference
     request_id bigint,
     -- when `true`, return immediately. when `false` wait for the request to complete before returning
     async bool default true
 )
     -- http response composite wrapped in a result type
-    returns net.http_response_result
+    returns http_response_result
     language plpgsql
 as $$
 declare
-    rec net._http_response;
+    rec _http_response;
     req_exists boolean;
 begin
 
     if not async then
-        perform net._await_response(request_id);
+        perform _await_response(request_id);
     end if;
 
     select *
     into rec
-    from net._http_response
+    from _http_response
     where id = request_id;
 
     if rec is null or rec.error_msg is not null then
@@ -319,7 +318,7 @@ begin
             'ERROR',
             coalesce(rec.error_msg, 'request matching request_id not found'),
             null
-        )::net.http_response_result;
+        )::http_response_result;
 
     end if;
 
@@ -331,27 +330,23 @@ begin
             rec.status_code,
             rec.headers,
             rec.content
-        )::net.http_response
-    )::net.http_response_result;
+        )::http_response
+    )::http_response_result;
 end;
 $$;
 
-create or replace function net.http_collect_response(
+create or replace function http_collect_response(
     -- request_id reference
     request_id bigint,
     -- when `true`, return immediately. when `false` wait for the request to complete before returning
     async bool default true
 )
     -- http response composite wrapped in a result type
-    returns net.http_response_result
+    returns http_response_result
     language plpgsql
 as $$
 begin
-  raise notice 'The net.http_collect_response function is deprecated.';
-  select net._http_collect_response(request_id, async);
+  raise notice 'The http_collect_response function is deprecated.';
+  select _http_collect_response(request_id, async);
 end;
 $$;
-
-grant usage on schema net to PUBLIC;
-grant all on all sequences in schema net to PUBLIC;
-grant all on all tables in schema net to PUBLIC;
