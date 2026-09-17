ffd461ded16c8c9e569f426d15c17dea1b3b13cc
Andrew Jackson <andrew.jackson@supabase.io>
2026-09-16 21:37:09 -0500

    chore: ruff --fix check

--- test/test_worker_behavior.py
+++ test/test_worker_behavior.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 import time
 import subprocess
 import os
-from common import http_request, http_requests, restart_worker, wait_for_any_response
+from common import http_requests, restart_worker, wait_for_any_response
 from common import wait_for_extension_drop, wait_for_postgres_ready
 from common import wait_for_queue_drain, wait_for_response_count
 from common import wait_for_worker_down, wait_for_worker_state
