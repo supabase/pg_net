68acdec7a5b652e7c9194241e6b8307b542b51f6
Andrew Jackson <andrew.jackson@supabase.io>
2026-09-23 11:24:14 -0500

    fix: add truncate/delete permission for single table

--- sql/pg_net--0.20.5--0.20.6.sql
+++ sql/pg_net--0.20.5--0.20.6.sql
@@ -2,8 +2,13 @@ revoke update on all sequences in schema net to PUBLIC;
 
 revoke insert on all tables in schema net to PUBLIC;
 revoke update on all tables in schema net to PUBLIC;
+
 revoke delete on all tables in schema net to PUBLIC;
+grant delete on net.http_request_queue to PUBLIC;
+
 revoke truncate on all tables in schema net to PUBLIC;
+grant truncate on net.http_request_queue to PUBLIC;
+
 revoke references on all tables in schema net to PUBLIC;
 revoke trigger on all tables in schema net to PUBLIC;
 revoke maintain on all tables in schema net to PUBLIC;
