ec49b0988b70888ed6659fdf3d84770492a10b06
Andrew Jackson <andrew.jackson@supabase.io>
2026-09-23 11:10:01 -0500

    fix: fix tests, format, convert tests to psycopg

--- test/test_privileges.py
+++ test/test_privileges.py
@@ -34,9 +34,12 @@ def test_net_on_pre_existing_role(conn):
     assert role[0][0] == "postgres"
 
     conn.execute("set local role to pre_existing;")
-    with pytest.raises(psycopg.errors.InsufficientPrivilege):
+    with pytest.raises(
+        psycopg.errors.InsufficientPrivilege,
+        match="permission denied for table http_request_queue",
+    ):
         conn.execute(
-                """
+            """
             select net.http_get(
                 'http://localhost:8080/anything'
             ), current_user;
@@ -44,57 +47,37 @@ def test_net_on_pre_existing_role(conn):
         )
 
 
-def test_net_on_new_role(sess):
-    """Check that a newly created role can use the net schema"""
+def test_net_on_new_role(conn):
+    """Check that a newly created role cannot use the net schema"""
 
-    role = sess.execute(text("select current_user;")).fetchone()
-    assert role[0] == "postgres"
+    role = conn.execute("select current_user;").fetchall()
+    assert role[0][0] == "postgres"
 
-    sess.execute(
-        text("""
-        create role another;
-    """)
-    )
-    sess.execute(text("set local role to another;"))
+    conn.execute("create role another;")
+    conn.commit()
+    conn.execute("set local role to another;")
 
-    (request_id, current_user) = sess.execute(
-        text(
+    with pytest.raises(
+        psycopg.errors.InsufficientPrivilege,
+        match="permission denied for table http_request_queue",
+    ):
+        conn.execute(
             """
-        select net.http_get(
-            'http://localhost:8080/anything'
-        ), current_user;
-    """
+            select net.http_get(
+                'http://localhost:8080/anything'
+            ), current_user;
+        """
         )
-    ).fetchone()
-    assert request_id == 1
-    assert current_user == "another"
 
-    # Commit to wakeup background worker
-    sess.commit()
+    conn.rollback()
 
-    # Confirm that the request was retrievable
-    sess.execute(text("set local role to another;"))
-    response = collect_response_sync(sess, request_id)
-    current_user = sess.execute(text("select current_user;")).scalar()
-    assert response["status"] == "SUCCESS"
-    assert current_user == "another"
-
-    sess.execute(text("set local role to another;"))
+    conn.execute("set local role to another;")
     # can use the net.worker_restart function
-    (res, current_user) = sess.execute(
-        text(
-            """
-        select net.worker_restart(), current_user;
-    """
-        )
-    ).fetchone()
-    assert res
-    assert current_user == "another"
+    res = conn.execute("select net.worker_restart(), current_user;").fetchone()
+    assert res[0]
+    assert res[1] == "another"
 
-    sess.execute(
-        text("""
-        select net.wait_until_running();
-        set local role postgres;
-        drop role another;
-    """)
-    )
+    conn.execute("select net.wait_until_running();")
+
+    conn.execute("set local role postgres;")
+    conn.execute("drop role another;")
